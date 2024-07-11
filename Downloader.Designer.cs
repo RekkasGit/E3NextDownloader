795fc74a18c5b72f53b52d1f96d740cba47c8573
Adam Liverman <livermana@gmail.com>
2024-07-11 00:55:28 -0400

    initial commit

---
+++ Downloader.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace E3NextDownloader
+{
+	partial class Downloader
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.checkBoxDownloadFramework = new System.Windows.Forms.CheckBox();
+			this.radioButtonLive = new System.Windows.Forms.RadioButton();
+			this.radioButtonEMU = new System.Windows.Forms.RadioButton();
+			this.buttonSelectFolder = new System.Windows.Forms.Button();
+			this.textBox_DownloadLocation = new System.Windows.Forms.TextBox();
+			this.buttonDownload = new System.Windows.Forms.Button();
+			this.labelStatus = new System.Windows.Forms.Label();
+			this.pictureBoxE3N = new System.Windows.Forms.PictureBox();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxE3N)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.checkBoxDownloadFramework);
+			this.groupBox1.Controls.Add(this.radioButtonLive);
+			this.groupBox1.Controls.Add(this.radioButtonEMU);
+			this.groupBox1.Location = new System.Drawing.Point(21, 24);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(233, 73);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Version";
+			// 
+			// checkBoxDownloadFramework
+			// 
+			this.checkBoxDownloadFramework.AutoSize = true;
+			this.checkBoxDownloadFramework.Checked = true;
+			this.checkBoxDownloadFramework.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBoxDownloadFramework.Location = new System.Drawing.Point(82, 43);
+			this.checkBoxDownloadFramework.Name = "checkBoxDownloadFramework";
+			this.checkBoxDownloadFramework.Size = new System.Drawing.Size(129, 17);
+			this.checkBoxDownloadFramework.TabIndex = 2;
+			this.checkBoxDownloadFramework.Text = "Download Framework";
+			this.checkBoxDownloadFramework.UseVisualStyleBackColor = true;
+			// 
+			// radioButtonLive
+			// 
+			this.radioButtonLive.AutoSize = true;
+			this.radioButtonLive.Location = new System.Drawing.Point(7, 43);
+			this.radioButtonLive.Name = "radioButtonLive";
+			this.radioButtonLive.Size = new System.Drawing.Size(45, 17);
+			this.radioButtonLive.TabIndex = 1;
+			this.radioButtonLive.Text = "Live";
+			this.radioButtonLive.UseVisualStyleBackColor = true;
+			// 
+			// radioButtonEMU
+			// 
+			this.radioButtonEMU.AutoSize = true;
+			this.radioButtonEMU.Checked = true;
+			this.radioButtonEMU.Location = new System.Drawing.Point(7, 20);
+			this.radioButtonEMU.Name = "radioButtonEMU";
+			this.radioButtonEMU.Size = new System.Drawing.Size(49, 17);
+			this.radioButtonEMU.TabIndex = 0;
+			this.radioButtonEMU.TabStop = true;
+			this.radioButtonEMU.Text = "EMU";
+			this.radioButtonEMU.UseVisualStyleBackColor = true;
+			// 
+			// buttonSelectFolder
+			// 
+			this.buttonSelectFolder.Location = new System.Drawing.Point(21, 103);
+			this.buttonSelectFolder.Name = "buttonSelectFolder";
+			this.buttonSelectFolder.Size = new System.Drawing.Size(111, 28);
+			this.buttonSelectFolder.TabIndex = 1;
+			this.buttonSelectFolder.Text = "Select Folder";
+			this.buttonSelectFolder.UseVisualStyleBackColor = true;
+			this.buttonSelectFolder.Click += new System.EventHandler(this.buttonSelectFolder_Click);
+			// 
+			// textBox_DownloadLocation
+			// 
+			this.textBox_DownloadLocation.Enabled = false;
+			this.textBox_DownloadLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox_DownloadLocation.Location = new System.Drawing.Point(21, 135);
+			this.textBox_DownloadLocation.Name = "textBox_DownloadLocation";
+			this.textBox_DownloadLocation.Size = new System.Drawing.Size(622, 29);
+			this.textBox_DownloadLocation.TabIndex = 2;
+			// 
+			// buttonDownload
+			// 
+			this.buttonDownload.Location = new System.Drawing.Point(21, 170);
+			this.buttonDownload.Name = "buttonDownload";
+			this.buttonDownload.Size = new System.Drawing.Size(111, 28);
+			this.buttonDownload.TabIndex = 3;
+			this.buttonDownload.Text = "Download!";
+			this.buttonDownload.UseVisualStyleBackColor = true;
+			this.buttonDownload.Click += new System.EventHandler(this.buttonDownload_Click);
+			// 
+			// labelStatus
+			// 
+			this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelStatus.Location = new System.Drawing.Point(-3, 201);
+			this.labelStatus.Name = "labelStatus";
+			this.labelStatus.Size = new System.Drawing.Size(776, 92);
+			this.labelStatus.TabIndex = 4;
+			// 
+			// pictureBoxE3N
+			// 
+			this.pictureBoxE3N.Location = new System.Drawing.Point(672, -2);
+			this.pictureBoxE3N.Name = "pictureBoxE3N";
+			this.pictureBoxE3N.Size = new System.Drawing.Size(117, 86);
+			this.pictureBoxE3N.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBoxE3N.TabIndex = 5;
+			this.pictureBoxE3N.TabStop = false;
+			// 
+			// Downloader
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(787, 294);
+			this.Controls.Add(this.pictureBoxE3N);
+			this.Controls.Add(this.labelStatus);
+			this.Controls.Add(this.buttonDownload);
+			this.Controls.Add(this.textBox_DownloadLocation);
+			this.Controls.Add(this.buttonSelectFolder);
+			this.Controls.Add(this.groupBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.Name = "Downloader";
+			this.Text = "E3N Downloader";
+			this.Load += new System.EventHandler(this.Downloader_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxE3N)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton radioButtonLive;
+		private System.Windows.Forms.RadioButton radioButtonEMU;
+		private System.Windows.Forms.Button buttonSelectFolder;
+		private System.Windows.Forms.TextBox textBox_DownloadLocation;
+		private System.Windows.Forms.Button buttonDownload;
+		private System.Windows.Forms.Label labelStatus;
+		private System.Windows.Forms.CheckBox checkBoxDownloadFramework;
+		private System.Windows.Forms.PictureBox pictureBoxE3N;
+	}
+}
+
---
+++ Downloader.cs
@@ -0,0 +1,591 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using E3NextLauncher.util;
+using Ionic.Zip;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Octokit;
+
+namespace E3NextDownloader
+{
+	public partial class Downloader : Form
+	{
+		string _versionNumber = "0.1";
+		string _extractionPath = "";
+		Task _downloadTask = null;
+		static byte[] _e3nImageBytes;
+		static Image _e3nImage;
+		public Downloader()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonSelectFolder_Click(object sender, EventArgs e)
+		{
+			var dialog = new CommonOpenFileDialog();
+			dialog.IsFolderPicker = true;
+			
+			using(new CenterWinDialog(this))
+			{
+				CommonFileDialogResult result = dialog.ShowDialog();
+				if (result == CommonFileDialogResult.Ok)
+				{
+					_extractionPath = dialog.FileName;
+					textBox_DownloadLocation.Text = _extractionPath;
+				}
+			}
+		}
+
+		private void DownloadFile(string url, string filename, string topic)
+		{
+			using (var client = new HttpClientDownloadWithProgress(url, filename))
+			{
+				client.ProgressChanged += (totalFileSize, totalBytesDownloaded, progressPercentage) =>
+				{
+					//need to update the UI, but on their thread, so need to invoke
+					this.labelStatus.Invoke((MethodInvoker)delegate
+					{
+						this.labelStatus.Text = $"Downloading {topic} {Math.Round(((Decimal)totalBytesDownloaded) / 1024 / 1024, 2)} Megabytes Downloaded";
+					});
+				};
+				client.StartDownload().Wait();
+			}
+		}
+
+		private bool FilesInFolder(string path)
+		{
+			DirectoryInfo dirInfo = new DirectoryInfo(path);
+			if(dirInfo.Exists ) {
+
+				var files = dirInfo.GetFiles();
+
+				if(files.Length > 0 )
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+		private bool EQGameInFolder(string path)
+		{
+			DirectoryInfo dirInfo = new DirectoryInfo(path);
+			if (dirInfo.Exists)
+			{
+
+				var files = dirInfo.GetFiles();
+
+				if (files.Length > 0)
+				{
+				
+					foreach(var file in files)
+					{
+						if (String.Compare(file.Name,"eqgame.exe", true)==0)
+						{
+							return true;
+						}
+					}
+				
+				}
+			}
+
+			return false;
+		}
+		private void DownloadEMU()
+		{
+
+			
+
+			string downloadLocation = $@"{_extractionPath}\download.zip";
+			string downloadURL = "https://github.com/RekkasGit/E3NextAndMQNextBinary/archive/refs/heads/main.zip";
+
+			DownloadFile(downloadURL, downloadLocation," E3Next+MQ ");
+
+			this.labelStatus.Invoke((MethodInvoker)delegate
+			{
+				this.labelStatus.Text = $"Done Downloading!";
+			});
+			System.Threading.Thread.Sleep(1000);
+
+			using (ZipFile zip = ZipFile.Read(downloadLocation))
+			{
+				var result = zip.Any(entry => entry.FileName.Contains("E3NextAndMQNextBinary-main"));
+				if (!result)
+				{
+					//some type of error
+					this.labelStatus.Invoke((MethodInvoker)delegate
+					{
+						this.labelStatus.Text = $"Error could not find Sub folder in zip file.";
+					});
+					return;
+				}
+				var selection = (from e in zip.Entries
+								 where (e.FileName).StartsWith("E3NextAndMQNextBinary-main/")
+
+								 select e);
+				this.labelStatus.Invoke((MethodInvoker)delegate
+				{
+					this.labelStatus.Text = $"Extracting files....";
+				});
+				//we don't want the github generated name, so we will just strip it out
+				
+				//selection count is updated each time we extract, so we  only increment when we are going to skip one
+				for (Int32 i = 0; i < selection.Count();)
+				{
+					var e = selection.ElementAt(i);
+					if (e.FileName == "E3NextAndMQNextBinary-main/")
+					{
+						i++;
+						continue;
+					}
+					e.FileName = e.FileName.Replace("E3NextAndMQNextBinary-main/", "");
+					this.labelStatus.Invoke((MethodInvoker)delegate
+					{
+						this.labelStatus.Text = $"Extracting {e.FileName}";
+					});
+					if(e.FileName.IndexOf("config/",0,StringComparison.OrdinalIgnoreCase)>-1)
+					{
+						e.Extract(_extractionPath, ExtractExistingFileAction.DoNotOverwrite);
+					}
+					else
+					{
+						e.Extract(_extractionPath, ExtractExistingFileAction.OverwriteSilently);
+					}
+				}
+			}
+
+			try
+			{
+				File.Delete(downloadLocation);
+			}
+			catch (Exception)
+			{
+
+			}
+			this.labelStatus.Invoke((MethodInvoker)delegate
+			{
+				this.labelStatus.Text = $"Done extracting!";
+			});
+			System.Threading.Thread.Sleep(1000);
+			this.labelStatus.Invoke((MethodInvoker)delegate
+			{
+				this.labelStatus.Text = $"";
+			});
+			this.buttonDownload.Invoke((MethodInvoker)delegate
+			{
+				this.buttonDownload.Enabled = true;
+			});
+			
+
+		}
+
+		
+		private void DownloadLive()
+		{
+
+			if(String.IsNullOrWhiteSpace(_extractionPath))
+			{
+				return;
+			}
+			if (!DownloadMQLive()) return;
+			if (!DownloadMQ2MonoLive()) return;
+			if (!DownloadMonoFrameworkLive()) return;
+			if (!DownloadE3Next()) return;
+
+			//give time for the user to read the message
+			System.Threading.Thread.Sleep(1000);
+			this.labelStatus.Invoke((MethodInvoker)delegate
+			{
+				this.labelStatus.Text = $"";
+			});
+			this.buttonDownload.Invoke((MethodInvoker)delegate
+			{
+				this.buttonDownload.Enabled = true;
+			});
+		}
+		private bool DownloadMQLive()
+		{
+			/*
+			 * Download Macroquest Live
+			 */
+
+			GitHubClient gitclient = new GitHubClient(new ProductHeaderValue("E3NextDownloader"));
+			var releases = gitclient.Repository.Release.GetAll("macroquest", "macroquest");
+			releases.Wait();
+			var latest = releases.Result.Where(x => x.TagName == "rel-live").First();
+			if (latest == null)
+			{
+				this.labelStatus.Invoke((MethodInvoker)delegate
+				{
+					this.labelStatus.Text = $"Issue finding Live tag for MQ";
+				});
+				return false;
+			}
+			var latestID = latest.Id;
+			var assets = gitclient.Repository.Release.GetAllAssets("macroquest", "macroquest", latestID).Result;
+			var zipFile = assets[0];
+
+
+			string downloadLocation = $@"{_extractionPath}\MQDownload.zip";
+			string downloadURL = zipFile.BrowserDownloadUrl;
+
+			DownloadFile(downloadURL, downloadLocation, " MacroQuest (Live)");
+			this.labelStatus.Invoke((MethodInvoker)delegate
+			{
+				this.labelStatus.Text = $"Done Downloading!";
+			});
+			System.Threading.Thread.Sleep(1000);
+			using (ZipFile zip = ZipFile.Read(downloadLocation))
+			{
+
+				this.labelStatus.Invoke((MethodInvoker)delegate
+				{
+					this.labelStatus.Text = $"Extracting files....";
+				});
+				//we don't want the github generated name, so we will just strip it out
+
+				foreach (var e in zip.Entries)
+				{
+					this.labelStatus.Invoke((MethodInvoker)delegate
+					{
+						this.labelStatus.Text = $"Extracting {e.FileName}";
+					});
+					e.Extract(_extractionPath, ExtractExistingFileAction.OverwriteSilently);
+				}
+
+
+			}
+			try { File.Delete(downloadLocation); } catch (Exception) { }
+
+			this.labelStatus.Invoke((MethodInvoker)delegate
+			{
+				this.labelStatus.Text = $"Done extracting!";
+			});
+			return true;
+		}
+		private bool DownloadMQ2MonoLive()
+		{
+			/*
+			 * Download MQ2Mono Live
+			 */
+			var gitclient = new GitHubClient(new ProductHeaderValue("E3NextDownloader"));
+			var releases = gitclient.Repository.Release.GetAll("RekkasGit", "MQ2Mono");
+			releases.Wait();
+			var latest = releases.Result.Where(x => x.TagName == "re-live").First();
+			if (latest == null)
+			{
+				this.labelStatus.Invoke((MethodInvoker)delegate
+				{
+					this.labelStatus.Text = $"Issue finding Live tag for MQ2Mono";
+				});
+				return false;
+			}
+			var latestID = latest.Id;
+			var assets = gitclient.Repository.Release.GetAllAssets("RekkasGit", "MQ2Mono", latestID).Result;
+			var zipFile = assets[0];
+
+
+			var downloadLocation = $@"{_extractionPath}\MQ2MonoDownload.zip";
+			var downloadURL = zipFile.BrowserDownloadUrl;
+
+			DownloadFile(downloadURL, downloadLocation, "MQ2Mono.");
+
+			using (ZipFile zip = ZipFile.Read(downloadLocation))
+			{
+
+				this.labelStatus.Invoke((MethodInvoker)delegate
+				{
+					this.labelStatus.Text = $"Extracting files....";
+				});
+				//we don't want the github generated name, so we will just strip it out
+
+				foreach (var e in zip.Entries)
+				{
+					this.labelStatus.Invoke((MethodInvoker)delegate
+					{
+						this.labelStatus.Text = $"Extracting {e.FileName}";
+					});
+					e.Extract(_extractionPath + @"\plugins\", ExtractExistingFileAction.OverwriteSilently);
+				}
+
+
+			}
+			try { File.Delete(downloadLocation); } catch (Exception) { }
+			return true;
+		}
+		private bool DownloadMonoFrameworkLive()
+		{
+			/*
+			 * Download Mono Framework x64
+			 */
+			if (checkBoxDownloadFramework.Checked)
+			{
+				var downloadLocation = $@"{_extractionPath}\MonoFrameworkx64Download.zip";
+				var downloadURL = @"https://github.com/RekkasGit/MQ2Mono-Framework64/archive/refs/heads/main.zip";
+
+				DownloadFile(downloadURL, downloadLocation, "Mono Framework x64.");
+
+				using (ZipFile zip = ZipFile.Read(downloadLocation))
+				{
+
+					this.labelStatus.Invoke((MethodInvoker)delegate
+					{
+						this.labelStatus.Text = $"Extracting files....";
+					});
+					//we don't want the github generated name, so we will just strip it out
+					var selection = (from e in zip.Entries
+									 where (e.FileName).StartsWith("MQ2Mono-Framework64-main/")
+
+									 select e);
+					for (Int32 i = 0; i < selection.Count();)
+					{
+						var e = selection.ElementAt(i);
+						if (e.FileName == "MQ2Mono-Framework64-main/" || e.FileName == "MQ2Mono-Framework64-main/README.md")
+						{
+							i++;
+							continue;
+						}
+						e.FileName = e.FileName.Replace("MQ2Mono-Framework64-main/", "");
+						this.labelStatus.Invoke((MethodInvoker)delegate
+						{
+							this.labelStatus.Text = $"Extracting {e.FileName}";
+						});
+						e.Extract(_extractionPath, ExtractExistingFileAction.OverwriteSilently);
+					}
+
+
+				}
+				try { File.Delete(downloadLocation); } catch (Exception) { }
+			}
+			return true;
+		}
+		private bool DownloadE3Next()
+		{
+			/*
+			 * Download the E3Next and Config folder
+			 */
+
+			var downloadLocation = $@"{_extractionPath}\E3NextDownload.zip";
+			var downloadURL = String.Empty;
+
+			var gitclient = new GitHubClient(new ProductHeaderValue("E3NextDownloader"));
+			var releases = gitclient.Repository.Release.GetAll("RekkasGit", "E3Next");
+			releases.Wait();
+			var latest = releases.Result.First();
+			var latestID = latest.Id;
+			var assets = gitclient.Repository.Release.GetAllAssets("RekkasGit", "E3Next", latestID).Result;
+
+			foreach (var asset in assets)
+			{
+				if (asset.Name == "e3.zip")
+				{
+					downloadURL = asset.BrowserDownloadUrl;
+					DownloadFile(downloadURL, downloadLocation, "E3Next");
+
+				}
+			}
+			if (String.IsNullOrEmpty(downloadURL))
+			{
+				this.labelStatus.Invoke((MethodInvoker)delegate
+				{
+					this.labelStatus.Text = $"Issue downloading E3Next";
+				});
+				this.buttonDownload.Invoke((MethodInvoker)delegate
+				{
+					this.buttonDownload.Enabled = true;
+				});
+				return false;
+			}
+			using (ZipFile zip = ZipFile.Read(downloadLocation))
+			{
+
+				this.labelStatus.Invoke((MethodInvoker)delegate
+				{
+					this.labelStatus.Text = $"Extracting files....";
+				});
+				//we don't want the github generated name, so we will just strip it out
+
+				foreach (var e in zip.Entries.ToList())
+				{
+					this.labelStatus.Invoke((MethodInvoker)delegate
+					{
+						this.labelStatus.Text = $"Extracting {e.FileName}";
+					});
+					if (e.FileName == @"e3/config.zip")
+					{
+						e.FileName = "config.zip";
+						e.Extract(_extractionPath);
+					}
+					else
+					{
+						e.Extract(_extractionPath + @"\mono\macros\", ExtractExistingFileAction.OverwriteSilently);
+
+					}
+				}
+				
+			}
+			using (ZipFile zip = ZipFile.Read(_extractionPath + @"\config.zip"))
+			{
+
+				this.labelStatus.Invoke((MethodInvoker)delegate
+				{
+					this.labelStatus.Text = $"Extracting files....";
+				});
+				//we don't want the github generated name, so we will just strip it out
+
+				foreach (var e in zip.Entries)
+				{
+
+					e.Extract(_extractionPath, ExtractExistingFileAction.DoNotOverwrite);
+
+
+				}
+
+			}
+			try { File.Delete(_extractionPath + @"\config.zip"); } catch (Exception) { }
+			try { File.Delete(downloadLocation); } catch (Exception) { }
+			return true;
+		}
+		private void buttonDownload_Click(object sender, EventArgs e)
+		{
+		
+			if (String.IsNullOrWhiteSpace(_extractionPath))
+			{
+				using(new CenterWinDialog(this))
+				{
+					MessageBox.Show("Please select a folder.");
+				}
+				
+				return;
+			}
+			
+			if(EQGameInFolder(_extractionPath))
+			{
+				using (new CenterWinDialog(this))
+				{
+					MessageBox.Show("Do not chose your EQ game folder. Please select another folder");
+				}
+				return;
+			}
+
+			if (FilesInFolder(_extractionPath) )
+			{
+				using (new CenterWinDialog(this))
+				{
+					if (MessageBox.Show(this, "Files exist in this directory do you wish to overwrite them? Generally better to go to a new folder.", "Overwrite files?", MessageBoxButtons.OKCancel) != DialogResult.OK)
+					{
+						return;
+					}
+				}
+			}
+
+
+			buttonDownload.Enabled = false;
+			if (radioButtonEMU.Checked)
+			{
+				_downloadTask = Task.Run(() => DownloadEMU());
+				return;
+			}
+			else if(radioButtonLive.Checked)
+			{
+				_downloadTask = Task.Run(() => DownloadLive());
+				return;
+			}
+			buttonDownload.Enabled = true;
+		}
+
+		private void Downloader_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				using (var filestream = File.OpenRead("E3Next.png"))
+				{
+					_e3nImageBytes = new byte[filestream.Length];
+					filestream.Read(_e3nImageBytes, 0, (Int32)filestream.Length);
+					//do stuff 
+				}
+
+				using (var stream = new MemoryStream(_e3nImageBytes))
+				{
+					_e3nImage = Image.FromStream(stream);
+				}
+				pictureBoxE3N.Image = _e3nImage;
+			}
+			catch (Exception ex)
+			{
+				System.Windows.Forms.MessageBox.Show(ex.Message);
+			}
+		}
+	}
+	/// <summary>
+	/// Code that can center any dialog/form/etch to the parent
+	/// </summary>
+	class CenterWinDialog : IDisposable
+	{
+		private int mTries = 0;
+		private Form mOwner;
+
+		public CenterWinDialog(Form owner)
+		{
+			mOwner = owner;
+			owner.BeginInvoke(new MethodInvoker(findDialog));
+		}
+
+		private void findDialog()
+		{
+			// Enumerate windows to find the message box
+			if (mTries < 0) return;
+			EnumThreadWndProc callback = new EnumThreadWndProc(checkWindow);
+			if (EnumThreadWindows(GetCurrentThreadId(), callback, IntPtr.Zero))
+			{
+				if (++mTries < 10) mOwner.BeginInvoke(new MethodInvoker(findDialog));
+			}
+		}
+		private bool checkWindow(IntPtr hWnd, IntPtr lp)
+		{
+			// Checks if <hWnd> is a dialog
+			StringBuilder sb = new StringBuilder(260);
+			GetClassName(hWnd, sb, sb.Capacity);
+			if (sb.ToString() != "#32770") return true;
+			// Got it
+			Rectangle frmRect = new Rectangle(mOwner.Location, mOwner.Size);
+			RECT dlgRect;
+			GetWindowRect(hWnd, out dlgRect);
+			MoveWindow(hWnd,
+				frmRect.Left + (frmRect.Width - dlgRect.Right + dlgRect.Left) / 2,
+				frmRect.Top + (frmRect.Height - dlgRect.Bottom + dlgRect.Top) / 2,
+				dlgRect.Right - dlgRect.Left,
+				dlgRect.Bottom - dlgRect.Top, true);
+			return false;
+		}
+		public void Dispose()
+		{
+			mTries = -1;
+		}
+
+		// P/Invoke declarations
+		private delegate bool EnumThreadWndProc(IntPtr hWnd, IntPtr lp);
+		[DllImport("user32.dll")]
+		private static extern bool EnumThreadWindows(int tid, EnumThreadWndProc callback, IntPtr lp);
+		[DllImport("kernel32.dll")]
+		private static extern int GetCurrentThreadId();
+		[DllImport("user32.dll")]
+		private static extern int GetClassName(IntPtr hWnd, StringBuilder buffer, int buflen);
+		[DllImport("user32.dll")]
+		private static extern bool GetWindowRect(IntPtr hWnd, out RECT rc);
+		[DllImport("user32.dll")]
+		private static extern bool MoveWindow(IntPtr hWnd, int x, int y, int w, int h, bool repaint);
+		private struct RECT { public int Left; public int Top; public int Right; public int Bottom; }
+	}
+}
